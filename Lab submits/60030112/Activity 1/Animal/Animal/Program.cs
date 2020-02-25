d819c15bc6bef60d30791e3a8273844908885781
AvirutRut <42264185+AvirutRut@users.noreply.github.com>
2020-02-25 15:50:21 +0700

    Export

---
+++ Lab submits/60030112/Activity 1/Animal/Animal/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+    class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("cat lol....");
+        }
+    }
+    class Dog :Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("dog big barking...");
+        }
+    }
+    class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}
