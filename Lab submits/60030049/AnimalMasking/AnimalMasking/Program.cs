06f517dec79252cc7036c08aeb1568eefe2a90ed
nantikan tippawan <60030049@kmitl.ac.th>
2020-02-25 16:23:57 +0700

    add

---
+++ Lab submits/60030049/AnimalMasking/AnimalMasking/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalMasking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Puppy puppy = new Puppy();
+            dog.Bark();
+            puppy.Bark();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("dog big barking...");
+        }  
+    }
+
+    class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+
+        new public void Bark()
+        {
+            Console.WriteLine("puppy small bark...");
+        }
+    }
+}
