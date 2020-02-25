0d1b3b561fea92f06c5613ebf426034979a1f289
AvirutRut <42264185+AvirutRut@users.noreply.github.com>
2020-02-25 16:38:49 +0700

    Export

---
+++ Lab submits/60030112/Activity 2/AnimalMasking/AnimalMasking/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalMasking
+{
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("dog big barking...");
+        }
+    }
+    class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+        new public void Bark()
+        {
+            Console.WriteLine("puppy small bark...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Puppy puppy = new Puppy();
+            dog.Bark();
+            puppy.Bark();
+        }
+    }
+}
