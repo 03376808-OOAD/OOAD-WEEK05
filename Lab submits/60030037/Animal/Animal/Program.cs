0ab51b2ffa01eafb6c094c1384c32b1a0dfeeabf
thanaponyalan <thanaponyalan@gmail.com>
2020-02-25 15:58:51 +0700

    animal.Eat

---
+++ Lab submits/60030037/Animal/Animal/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Animal
+{
+    class Animal
+    {
+        public void Eat()
+        {
+
+        }
+    }
+    class Cat : Animal
+    {
+        public void Meow()
+        {
+
+        }
+    }
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("dog big barking...");
+        }
+    }
+    class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Eat();
+            Console.ReadKey();
+        }
+    }
+}
