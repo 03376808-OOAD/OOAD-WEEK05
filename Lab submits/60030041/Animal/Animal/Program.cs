2c327482c9fba3e2d2fd764b2e297dedaeb9d2b5
tanakorn128 <42264080+tanakorn128@users.noreply.github.com>
2020-02-25 16:14:19 +0700

    pic01

---
+++ Lab submits/60030041/Animal/Animal/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Animal
+    {
+        public void Eat()
+        {
+
+        }
+    }
+    class Cat : Animal
+    {
+        public void Meow()
+        {
+
+        }
+    }
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("dog big barking...");
+        }
+    }
+    class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Eat();
+            Console.ReadKey();
+        }
+    }
+}
