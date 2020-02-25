b47d611221754dcbdca8153640b233078d64e980
nantikan tippawan <60030049@kmitl.ac.th>
2020-02-25 15:43:18 +0700

    A1

---
+++ Lab submits/60030049/Animal/Animal/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Dog dog = new Dog();
+           Puppy puppy = new Puppy();
+           // Cat cat = new Cat();
+            //Animal animal = new Animal();
+            //dog.Bark();
+           puppy.Bark();
+           puppy.Eat();
+            puppy.Weep();
+           //animal.Eat();
+           //cat.Eat();
+           // cat.Meow();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("Moew...");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("dog big barking...");
+        }
+    }
+    class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+        new public void Bark()
+        {
+            Console.WriteLine("puppy small bark...");
+        }
+    }
+}
