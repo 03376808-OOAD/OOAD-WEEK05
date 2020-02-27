2d607640ea10ca56ed43769c55098caffb7bdff9
boaTTTTTT <53853565+boaTTTTTT@users.noreply.github.com>
2020-02-27 20:16:33 +0700

    60030089

---
+++ Lab submits/60030089/Act1/Animal/Animal/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animal
+{
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("I am animal,I can eat.");
+        }
+    }
+    class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("I am cat,I can Meow.");
+        }
+    }
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("dog big barking....");
+        }
+    }
+    class Puppy : Dog 
+    {
+        public void Weep()
+        {
+            Console.WriteLine("Weeping...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[]args)
+        {
+
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+
+            Console.ReadLine();
+        }
+    }
+  
+}
