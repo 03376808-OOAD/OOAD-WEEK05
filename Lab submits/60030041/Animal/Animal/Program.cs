c8122bbf5d48329a283156426fe9f27e4bad9c23
tanakorn128 <42264080+tanakorn128@users.noreply.github.com>
2020-02-29 22:28:37 +0700

    act2

--- Lab submits/60030041/Animal/Animal/Program.cs
+++ Lab submits/60030041/Animal/Animal/Program.cs
@@ -6,43 +6,10 @@ using System.Threading.Tasks;
 
 namespace Animal
 {
-    class Animal
-    {
-        public void Eat()
-        {
-            Console.WriteLine("Eat...");
-        }
-    }
-    class Cat : Animal
-    {
-        public void Meow()
-        {
-            Console.WriteLine("Meow");
-        }
-    }
-    class Dog : Animal
-    {
-        public void Bark()
-        {
-            Console.WriteLine("dog big barking...");
-        }
-    }
-    class Puppy : Dog
-    {
-        public void Weep()
-        {
-            Console.WriteLine("weeping...");
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Puppy puppy = new Puppy();
-            puppy.Eat();
-            puppy.Bark();
-            puppy.Weep();
-            Console.ReadKey();
         }
     }
 }
