666c114faea8f1bc2b2c7a7c8bdde68b1580c87b
Koson Trachu <koson.trachu@gmail.com>
2020-03-10 06:37:57 +0700

    add example

---
+++ Lab-examples/Lab5-Activity3/Lab5-Activity3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_Activity3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class ParentClass
+    {
+        public ParentClass()
+        {
+            Console.WriteLine("Parent Constructor.");
+        }
+
+        public void print()
+        {
+            Console.WriteLine("I'm a Parent Class.");
+        }
+    }
+
+}
