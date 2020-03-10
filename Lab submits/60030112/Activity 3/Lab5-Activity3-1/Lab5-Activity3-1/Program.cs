fb8f9047e06b503ed61c56c50b3750735457a538
AvirutRut <42264185+AvirutRut@users.noreply.github.com>
2020-03-10 16:46:50 +0700

    go

---
+++ Lab submits/60030112/Activity 3/Lab5-Activity3-1/Lab5-Activity3-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_Activity3_1
+{
+    public class ParentClass
+    {
+        public ParentClass()
+        {
+            Console.WriteLine("Parent Constructor");
+        }
+        public void print()
+        {
+            Console.WriteLine("I'm a Parent Class");
+        }
+    }
+    public class ChildClass : ParentClass
+    {
+        public ChildClass()
+        {
+            Console.WriteLine("Child Constructor");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildClass child = new ChildClass();
+            child.print();
+        }
+    }
+}
