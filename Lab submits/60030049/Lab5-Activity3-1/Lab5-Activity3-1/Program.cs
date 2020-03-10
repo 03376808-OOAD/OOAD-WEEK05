8998700e42bf231ad1abf02972dc86b89e3531a1
nantikan tippawan <60030049@kmitl.ac.th>
2020-03-10 17:12:26 +0700

    up

---
+++ Lab submits/60030049/Lab5-Activity3-1/Lab5-Activity3-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_Activity3_1
+{
+    class Program
+    {
+        public class ParentClass
+        {       
+                public ParentClass()
+                {
+                    Console.WriteLine("Parent Constructor.");
+                }
+                public void print()
+                {
+                    Console.WriteLine("I'm a Parent Class.");
+                }
+        }
+
+        public class ChildClass : ParentClass
+        {
+                public ChildClass()
+                {
+                    Console.WriteLine("Child Constructor.");
+                }
+        }
+        static void Main(string[] args)
+        {
+                ChildClass child = new ChildClass();
+                child.print();
+
+        }
+    }
+}
