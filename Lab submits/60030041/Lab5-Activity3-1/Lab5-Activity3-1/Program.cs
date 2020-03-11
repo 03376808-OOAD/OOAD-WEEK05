71f29aeb4c8afab9dede5bd4c63f51123af7dd7b
tanakorn128 <42264080+tanakorn128@users.noreply.github.com>
2020-03-12 01:12:51 +0700

    update

---
+++ Lab submits/60030041/Lab5-Activity3-1/Lab5-Activity3-1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_Activity3_1
+{
+    class Program
+    {
+        public class ParentClass
+        {
+            public ParentClass()
+            {
+                Console.WriteLine("Parent Constructor.");
+            }
+            public void print()
+            {
+                Console.WriteLine("I'm aParent Class");
+            }
+        }
+        public class ChildClass : ParentClass
+        {
+            public ChildClass()
+            {
+                Console.WriteLine("Child Constructor.");
+            }
+        }
+        static void Main(string[] args)
+        {
+            ChildClass child = new ChildClass();
+            child.print();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab submits/60030041/Lab5-Activiy4-1/Lab5-Activiy4-1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_Activiy4_1
+{
+    class Program
+    {
+        public class DrawingObject
+        {
+            public virtual void Draw()
+            {
+                Console.WriteLine("I'm just a generic drawing object.");
+            }
+        }
+        public class Circle : DrawingObject
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("I.m a Circle.");
+            }
+        }
+        public class Line : DrawingObject
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("I,m a Line.");
+            }
+        }
+        public class Square : DrawingObject
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("I'm a Square.");
+            }
+        }
+        public static int Main(string[] args)
+        {
+            DrawingObject[] aObj = new DrawingObject[4];
+            aObj[0] = new Line();
+            aObj[1] = new Circle();
+            aObj[2] = new Square();
+            aObj[3] = new DrawingObject();
+            foreach(DrawingObject drawObj in aObj)
+            {
+                drawObj.Draw();
+                
+            }
+            Console.ReadKey();
+            return 0;
+            
+        }
+    }
+}
