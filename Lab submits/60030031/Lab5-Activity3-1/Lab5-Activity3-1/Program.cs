f3604f5f041feb3118f2f33b60744eedc3b2bc66
thanakamon <42264205+thanakamon@users.noreply.github.com>
2020-03-13 16:21:08 +0700

    all act

---
+++ Lab submits/60030031/Lab5-Activity3-1/Lab5-Activity3-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_Activity3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildClass child = new ChildClass();
+
+            child.print();
+        }
+    }
+    public class ParentClass
+    {
+        public ParentClass()
+        {
+            Console.WriteLine("Parent Constructor.");
+        }
+
+        public void print()
+        {
+            Console.WriteLine("I'm a Parent Class.");
+        }
+    }
+    public class ChildClass : ParentClass
+    {
+        public ChildClass()
+        {
+            Console.WriteLine("Child Constructor.");
+        }
+    }
+}
---
+++ Lab submits/60030031/Lab5-Activiy4-2/Lab5-Activiy4-2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5_Activiy4_1
+{
+    class Program
+    {
+        public static int Main()
+        {
+
+            // 1. สร้างอาร์เรย์ของออปเจกต์ของ base class มีพื้นที่เก็บ 5 ออปเจ็กต์ 
+            DrawingObject[] dObj = new DrawingObject[5];
+
+
+            // 1. สร้างออปเจกต์จาก derived class แต่ละตัวแล้วเก็บ reference ในอาร์เรย์ 
+            dObj[0] = new Line();
+            dObj[1] = new Circle();
+            dObj[2] = new Square();
+            dObj[3] = new Rectangle();
+            dObj[4] = new DrawingObject();
+
+            // วนลูปเพื่อเรียกเมธอดในแต่ละออปเจ็กต์
+            foreach (DrawingObject drawObj in dObj)
+            {
+                drawObj.Draw();
+            }
+
+            return 0;
+        }
+        public class DrawingObject
+        {
+            public virtual void Draw()
+            {
+                Console.WriteLine("I'm just a generic drawing object.");
+            }
+        }
+        public class Circle : DrawingObject
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("I'm a Circle.");
+            }
+        }
+        public class Line : DrawingObject
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("I'm a Line.");
+            }
+        }
+        public class Square : DrawingObject
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("I'm a Square.");
+            }
+        }
+        public class Rectangle : Square
+        {
+            public new void Draw()
+            {
+                Console.WriteLine("I'm a rectangle.");
+            }
+        }
+    }
+}
