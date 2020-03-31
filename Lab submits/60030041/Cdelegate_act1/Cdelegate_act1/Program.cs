fecdb3b8311a8014b9e6ea4647c97936eab9b6d1
tanakorn128 <42264080+tanakorn128@users.noreply.github.com>
2020-03-31 17:00:38 +0700

    finished

---
+++ Lab submits/60030041/Cdelegate_act1/Cdelegate_act1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cdelegate_act1
+{
+    delegate void delehello();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            delehello delH = hello;
+            delH();Console.ReadKey();
+        } 
+        public static void hello()
+        {
+            Console.WriteLine("hrllo world.");
+        }
+    }
+}
