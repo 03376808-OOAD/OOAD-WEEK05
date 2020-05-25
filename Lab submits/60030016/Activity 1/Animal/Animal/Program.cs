49022135fc534b366e5591dca38b5034a58ed94c
JirawatChairongsri <42440557+JirawatChairongsri@users.noreply.github.com>
2020-05-25 08:17:43 +0700

    1

---
+++ Lab submits/60030016/Activity 1/Animal/Animal/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Animal
+{
+    class Animal            // เพิ่ม class ชื่อ  Animal
+    {
+        public void Eat()   // เมธอด Eat()
+        {
+            // ยังไม่ต้องเขียน Code
+        }
+    }
+    class Cat : Animal
+    {
+        public void Meow()
+        {
+
+        }
+    }
+
+    class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("dog big barking...");
+        }
+    }
+
+    class Puppy : Dog
+    {
+        public void Weep()
+        {
+            Console.WriteLine("weeping...");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}
